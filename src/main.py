9d2996d97a765121c455c263e73cf95703506a6e
juanmabv <juanmabatistav@gmail.com>
2023-06-13 15:58:09 +0100

    quinto commit

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ async def main():
     async with Actor:
         # Read the Actor input
         actor_input = await Actor.get_input() or {}
-        start_urls = actor_input.get('start_urls', [{ 'url': 'https://apify.com' }])
-        max_depth = actor_input.get('max_depth', 1)
+        start_urls = actor_input.get('start_urls')
+        max_depth = actor_input.get('max_depth')
 
         if not start_urls:
             Actor.log.info('No start URLs specified in actor input, exiting...')
@@ -36,41 +36,16 @@ async def main():
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=chrome_options)
-
-        driver.get('http://www.example.com')
-        assert driver.title == 'Example Domain'
-
-        # Process the requests in the queue one by one
-        while request := await default_queue.fetch_next_request():
-            url = request['url']
-            depth = request['userData']['depth']
-            Actor.log.info(f'Scraping {url} ...')
-
-            try:
-                # Open the URL in the Selenium WebDriver
-                driver.get(url)
-                print('hola')
-                print('hola2')
-
-                # If we haven't reached the max depth,
-                # look for nested links and enqueue their targets
-                if depth < max_depth:
-                    for link in driver.find_elements(By.TAG_NAME, 'a'):
-                        link_href = link.get_attribute('href')
-                        link_url = urljoin(url, link_href)
-                        if link_url.startswith(('http://', 'https://')):
-                            Actor.log.info(f'Enqueuing {link_url} ...')
-                            await default_queue.add_request({
-                                'url': link_url,
-                                'userData': {'depth': depth + 1 },
-                            })
-
-                # Push the title of the page into the default dataset
-                title = driver.title
-                await Actor.push_data({ 'url': url, 'title': title })
-            except:
-                Actor.log.exception(f'Cannot extract data from {url}.')
-            finally:
-                await default_queue.mark_request_as_handled(request)
+        
+        try:
+            # Open the URL in the Selenium WebDriver
+            driver.get(url)
+            print('hola')
+            print('hola2')
+            await Actor.push_data({ 'url': url, 'title': title })
+        except:
+            Actor.log.exception(f'Cannot extract data from {url}.')
+        finally:
+            await default_queue.mark_request_as_handled(request)
 
         driver.quit()
