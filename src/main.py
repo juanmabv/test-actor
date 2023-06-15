087039cf78b8024d5086e6eab1aeadfe87169758
juanmabv <juanmabatistav@gmail.com>
2023-06-15 21:40:28 +0100

    sce_v1

--- src/main.py
+++ src/main.py
@@ -119,12 +119,10 @@ async def main():
                         time.sleep(1)
                         titulo_oferta = oferta.find_element(
                             By.CSS_SELECTOR, 'h6').get_attribute('innerText')
-                        lista_titulo.append(titulo_oferta)
                         print(titulo_oferta) if verbose else 1
 
                         fecha_oferta = oferta.find_element(
                             By.CSS_SELECTOR, 'ul > li:nth-child(1)').get_attribute('innerText').split(': ')[-1]
-                        lista_fecha.append(fecha_oferta)
                         print(fecha_oferta) if verbose else 1
 
                         enlace_modal = oferta.find_element(
@@ -137,8 +135,9 @@ async def main():
                         print(id_oferta) if verbose else 1
 
                         enlace_oferta = f'https://www3.gobiernodecanarias.org/empleo/portal/web/sce/servicios/ofertas/ofertas_empleo/{id_oferta}'
-                        lista_enlaces.append(enlace_oferta)
                         print(enlace_oferta) if verbose else 1
+
+                        await Actor.push_data({'enlace': enlace_oferta, 'fecha': fecha_oferta, 'titulo': titulo_oferta})
 
                         webdriver.ActionChains(driver).send_keys(
                             Keys.ESCAPE).perform()
@@ -159,29 +158,21 @@ async def main():
                         seguir = False
                         print('no hay página siguiente')
 
-                df_final = pd.DataFrame({
-                    'titulo': lista_titulo,
-                    'enlace': lista_enlaces,
-                    'fecha': lista_fecha,
-                })
+            # data = {
+            #     'booleano': [True, False, False, True, True, False, False, True, False, False, True, False, True, False, True],
+            #     'texto': ['Hola', 'Mundo', 'Python', 'Data', 'Science', 'OpenAI', 'ChatGPT', 'Pandas', 'DataFrame', 'Ejemplo', 'Columna', 'Número', 'Fecha', 'Hora', 'Datetime'],
+            #     'entero': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            #     'decimal': [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0, 11.1, 12.2, 13.3, 14.4, 15.5],
+            #     'fecha': pd.date_range('2023-01-01', periods=15),
+            #     'hora': pd.date_range('00:00:00', periods=15, freq='H'),
+            #     'fecha_hora': pd.date_range('2023-01-01 00:00:00', periods=15, freq='H')
+            # }
 
-            print(df_final.head())
+            # df = pd.DataFrame(data)
 
-            data = {
-                'booleano': [True, False, False, True, True, False, False, True, False, False, True, False, True, False, True],
-                'texto': ['Hola', 'Mundo', 'Python', 'Data', 'Science', 'OpenAI', 'ChatGPT', 'Pandas', 'DataFrame', 'Ejemplo', 'Columna', 'Número', 'Fecha', 'Hora', 'Datetime'],
-                'entero': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                'decimal': [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0, 11.1, 12.2, 13.3, 14.4, 15.5],
-                'fecha': pd.date_range('2023-01-01', periods=15),
-                'hora': pd.date_range('00:00:00', periods=15, freq='H'),
-                'fecha_hora': pd.date_range('2023-01-01 00:00:00', periods=15, freq='H')
-            }
-
-            df = pd.DataFrame(data)
-
-            for _, row in df.iterrows():
-                data_dict = row.to_dict()
-                await Actor.push_data(data_dict)
+            # for _, row in df.iterrows():
+            #     data_dict = row.to_dict()
+            #     await Actor.push_data(data_dict)
         except:
             Actor.log.exception(f'Cannot extract data from {url}.')
 
